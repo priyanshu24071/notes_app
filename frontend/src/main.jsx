23edb989baed879ee548fb51c9bfbcdce8cf1d30
Priyanshu <priyanshu@podworld.in>
2025-04-17 14:16:46 +0530

    vercel fix

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,14 @@
+// frontend/src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css'; 
+import axios from 'axios';             
+import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
 import { BrowserRouter } from 'react-router-dom';
+
+axios.defaults.baseURL = import.meta.env.VITE_API_BASE;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
